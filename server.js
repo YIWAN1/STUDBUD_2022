b948620dd55ae419b51de2457facc3d945c55af4
YI WAN <ywan5525@uni.sydney.edu.au>
2022-04-29 10:46:44 +0800

    Improving Backend Dev: Parcel

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/dist'));
 
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 
 
